a3a43a75bee5edbba921d635018a27a30a7c1c77
Sebastian <novvac21@protonmail.com>
2021-04-16 17:16:17 +0200

    created base button and added drawer options

--- client/src/components/base/index.ts
+++ client/src/components/base/index.ts
@@ -1,4 +1,6 @@
 import Vue from 'vue';
 import BaseInput from './Input.vue';
+import BaseButton from './Button.vue';
 
 Vue.component("BaseInput", BaseInput);
+Vue.component("BaseButton", BaseButton);
--- client/src/components/core/Drawer.vue
+++ client/src/components/core/Drawer.vue
@@ -2,11 +2,29 @@
     <v-navigation-drawer
         app
         right
-        width="32%"
-        class="pa-16"
+        width="36%"
+        class="pa-16 drawer"
         floating
     >
-        NAVIGATION DRAWER
+        <v-row class="ma-0">
+            <base-button
+                v-for="(btn, i) in buttons"
+                :key="i"
+                text
+                class="body-1 text-none mr-3"
+            >
+                {{btn.text}}
+
+                <v-chip 
+                    v-if="btn.badge"
+                    x-small
+                    label
+                    class="ml-3 px-2 primary"
+                >
+                    {{btn.chip}}
+                </v-chip>
+            </base-button>
+        </v-row>
     </v-navigation-drawer>
 </template>
 
@@ -15,5 +33,15 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 
 @Component
-export default class Drawer extends Vue {}
+export default class Drawer extends Vue {
+    buttons = [
+        {
+            text: "Notifications",
+            chip: 1,
+        },
+        {
+            text: "Places",
+        }
+    ]
+}
 </script>
