1a3ebf501066526935dac44ca795032090bfe410
Sebastian <novvac21@protonmail.com>
2021-04-16 23:54:42 +0200

    added vue particles

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,8 +4,10 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import './components/base/index';
+import VueParticles from 'vue-particles';
 
 Vue.config.productionTip = false
+Vue.use(VueParticles);
 
 new Vue({
   router,
--- client/src/views/Auth.vue
+++ client/src/views/Auth.vue
@@ -1,6 +1,31 @@
 <template>
     <div class="auth">
-        auth
+        <v-row class="ma-0">
+            <v-col cols="5" class="red lighten-1 d-none d-md-block pa-0">
+                <vue-particles
+                    style="height: 100%; max-height: 100vh; padding: 0; margin: 0;"
+                    color="#dedede"
+                    :particleOpacity=".75"
+                    :particlesNumber="512"
+                    shapeType="circle"
+                    :particleSize="4"
+                    linesColor="#e9e9e9"
+                    :linesWidth="1"
+                    :lineLinked="true"
+                    :lineOpacity="0.4"
+                    :linesDistance="30"
+                    :moveSpeed="1"
+                    :hoverEffect="true"
+                    hoverMode="grab"
+                    :clickEffect="false"
+                >
+                </vue-particles>
+            </v-col>
+
+            <v-col cols="12" :md="7">
+                second column
+            </v-col>
+        </v-row>
     </div>
 </template>
 
@@ -11,3 +36,13 @@ import Component from 'vue-class-component';
 @Component
 export default class Auth extends Vue {}
 </script>
+
+<style lang="scss">
+.auth {
+    height: 100%;
+
+    .row {
+        height: 100%;
+    }
+}
+</style>
