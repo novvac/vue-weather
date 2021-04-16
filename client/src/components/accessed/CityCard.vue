3f5230758e7a939cbd984feb8dc4821a1eefabb7
Sebastian <novvac21@protonmail.com>
2021-04-16 19:16:58 +0200

    updated city card

---
+++ client/src/components/accessed/CityCard.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-card
+        :width="width"
+        :height="height"
+        class="city-card text-center"
+        flat
+        color="transparent"
+    >
+        <div class="image" :style="'background: url(' + city.src + ')'"/>
+        
+        <p class="mt-3" :style="{paddingTop: height + 'px'}">
+            {{city.city}}, {{city.country}}
+        </p>
+    </v-card>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class CityCard extends Vue {
+    @Prop(Object) readonly city: object | undefined
+
+    height = 256
+    width = 196
+}
+</script>
+
+<style lang="scss">
+
+.city-card {
+    position: relative;
+    cursor: pointer;
+
+    &:hover {
+        div.image {
+            height: calc(100% + 16px);
+            transform: translate(0,-24px);
+            box-shadow: 0 4px 6px rgba(0,0,0,.1);
+        }
+    }
+    div.image {
+        position: absolute;
+        transition: 300ms !important;
+        border-radius: 24px !important;
+        height: 100%;
+        width: 100%;
+        background-size: cover !important;
+    }
+    p {
+        font-weight: 500;
+    }
+}
+</style>
--- client/src/components/core/Main.vue
+++ client/src/components/core/Main.vue
@@ -15,9 +15,18 @@
                 />
             </v-row>
 
-            <div class="my-10 display-1">
+            <div class="mt-10 mb-12 display-1">
                 Weather <b>Forecast</b>
             </div>
+
+            <v-row class="ma-0 cities">
+                <city-card
+                    v-for="item in cities"
+                    :key="item.city"
+                    :city="item"
+                    class="mr-12"
+                />
+            </v-row>
         </v-container>
     </v-main>
 </template>
@@ -26,12 +35,40 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 
-@Component
-export default class Main extends Vue {}
+import CityCard from '../accessed/CityCard.vue';
+
+@Component({
+    components: {
+        CityCard,
+    }
+})
+export default class Main extends Vue {
+    cities = [
+        {
+            src: "https://i.pinimg.com/originals/21/eb/1f/21eb1f1de25367847e8b41a9149db65a.jpg",
+            city: "Paris",
+            country: "France"
+        },
+        {
+            src: "https://upload.wikimedia.org/wikipedia/commons/8/82/London_Big_Ben_Phone_box.jpg",
+            city: "London",
+            country: "UK"
+        },
+    ]
+}
 </script>
 
 <style lang="scss">
 .search-input {
     flex: .2 1 auto !important;
+}
+.cities {
+    max-width: calc(100% - 160px - 1rem);
+
+    .city-card {
+        &:last-child {
+            margin-right: 0 !important;
+        }
+    }
 }
 </style>
