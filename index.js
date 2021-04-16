8ff236603936f849ed0b88a3bfd0529c649208b1
Sebastian <novvac21@protonmail.com>
2021-04-17 01:56:12 +0200

    added mongoose and connected to local databse

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
+import keys from './config/keys.js';
 
 // routing
 import auth from './routes/auth.routing.js';
@@ -13,6 +15,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors());
 app.use(morgan("tiny"));
+
+mongoose.connect(keys.mongoURI, {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connection.on("error", console.error.bind(console, "connection error:"));
+mongoose.connection.once('open', () => {
+    console.log("MongoDB", "Database was connected!");
+})
 
 // api
 app.use("/api/v1/auth", auth);
