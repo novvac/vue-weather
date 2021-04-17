4207811de570400f701f01ba583e92c972d55677
Sebastian <novvac21@protonmail.com>
2021-04-17 14:35:29 +0200

    added progress circular when user loading

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,37 @@
 <template>
   <v-app>
-    <router-view/>
+    <router-view v-if="!loading"/>
+    <div v-else class="d-flex justify-center align-center" style="height: 100%">
+      <v-progress-circular indeterminate size="128" width="2"></v-progress-circular>
+    </div>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import store from './store/index';
 
 export default Vue.extend({
   name: 'App',
-  data: () => ({
-    //
-  })
+  data() {
+    return {
+      loading: true,
+    }
+  },
+  created() {
+    if(store.state.isAuthenticated === undefined) {
+      this.$http.get("/api/user/")
+        .then(res => {
+          console.log(res);
+          this.loading = false;
+        })
+        .catch(err => {
+          console.log(err.response);
+          this.loading = false;
+        })
+    } else {
+      this.loading = false;
+    }
+  }
 })
 </script>
