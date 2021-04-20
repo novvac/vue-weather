dd7a8a31e4b9351a2cc63109858c02144d06ec47
Sebastian <novvac21@protonmail.com>
2021-04-20 19:25:09 +0200

    updated charts

--- client/src/components/accessed/WeatherCharts.vue
+++ client/src/components/accessed/WeatherCharts.vue
@@ -1,50 +1,5 @@
 <template>
     <div class="mr-16">
-        <div>
-            <p class="mt-16 overline">Temperature in current week</p>
-            <apexchart
-                type="line" 
-                :options="{
-                    ...options,
-                    yaxis: {
-                        labels: {
-                            formatter: function (value) {
-                                return `${value.toFixed(2)} °C`;
-                            }
-                        },
-                    },
-                }" 
-                :series="[{
-                    name: 'Dzień',
-                    data: weatherData[activeCity].daily.map(el => el.temp.day.toFixed(2))
-                }, {
-                    name: 'Noc',
-                    data: weatherData[activeCity].daily.map(el => el.temp.night.toFixed(2))
-                }]"
-            />
-        </div>
-
-        <div>
-            <p class="mt-16 overline">Humidity in current week</p>
-            <apexchart
-                type="line" 
-                :options="{
-                    ...options,
-                    yaxis: {
-                        labels: {
-                            formatter: function (value) {
-                                return `${value.toFixed(2)} %`;
-                            }
-                        },
-                    },
-                }" 
-                :series="[{
-                    name: 'Dzień',
-                    data: weatherData[activeCity].daily.map(el => el.humidity)
-                }]"
-            />
-        </div>
-
         <div v-if="weatherData[activeCity] && activeDay === 0">
             <p class="mt-16 overline">Precipitation for the next hour</p>
             <apexchart
@@ -66,8 +21,80 @@
                     }
                 }" 
                 :series="[{
-                    name: 'Opady (mm)',
+                    name: 'Precipitation (mm)',
                     data: weatherData[activeCity].minutely.map(el => el.precipitation.toFixed(2)).splice(0,60)
+                }]"
+            />
+        </div>
+
+        <div v-if="weatherData[activeCity] && [0,1].includes(activeDay)">
+            <p class="mt-16 overline">Temperature for the next 48h</p>
+            <apexchart
+                type="area" 
+                :options="{
+                    ...options,
+                    yaxis: {
+                        labels: {
+                            formatter: function (value) {
+                                return `${value.toFixed(1)} °C`;
+                            }
+                        },
+                    },
+                    xaxis: {
+                        labels: {
+                            show: false,
+                        },
+                        categories: next48h
+                    }
+                }" 
+                :series="[{
+                    name: 'Temperature',
+                    data: weatherData[activeCity].hourly.map(el => el.temp)
+                }]"
+            />
+        </div>
+
+        <div>
+            <p class="mt-16 overline">Temperature in current week</p>
+            <apexchart
+                type="line" 
+                :options="{
+                    ...options,
+                    yaxis: {
+                        labels: {
+                            formatter: function (value) {
+                                return `${value.toFixed(1)} °C`;
+                            }
+                        },
+                    },
+                }" 
+                :series="[{
+                    name: 'Day',
+                    data: weatherData[activeCity].daily.map(el => el.temp.day.toFixed(2))
+                }, {
+                    name: 'Night',
+                    data: weatherData[activeCity].daily.map(el => el.temp.night.toFixed(2))
+                }]"
+            />
+        </div>
+
+        <div>
+            <p class="mt-16 overline">Humidity in current week</p>
+            <apexchart
+                type="area" 
+                :options="{
+                    ...options,
+                    yaxis: {
+                        labels: {
+                            formatter: function (value) {
+                                return `${value.toFixed(1)} %`;
+                            }
+                        },
+                    },
+                }" 
+                :series="[{
+                    name: 'Day',
+                    data: weatherData[activeCity].daily.map(el => el.humidity)
                 }]"
             />
         </div>
@@ -81,6 +108,7 @@ import store from '../../store/index';
 @Component
 export default class WeatherCharts extends Vue {
     categories = [];
+    next48h = [];
 
     get activeCity() { return store.getters.activeCity }
     get activeDay() { return store.getters.activeDay }
@@ -92,13 +120,31 @@ export default class WeatherCharts extends Vue {
             let hours = initialDate.getHours();
             let minutes = initialDate.getMinutes();
 
+            for(let i=0; i<48; i++) {
+                if(hours > 23) {
+                    hours = 0;
+                    minutes = 0;
+                }
+
+                this.next48h.push(`${hours >= 10 ? hours : '0'+hours}:00`);
+
+                hours += 1;
+            }
+
+            hours = initialDate.getHours();
+            minutes = initialDate.getMinutes();
             for(let i=0; i<60; i++) {
                 if(minutes > 59) {
                     hours += 1;
                     minutes = 0;
                 }
+
+                if(hours > 23) {
+                    hours = 0;
+                    minutes = 0;
+                }
                 
-                this.categories.push(`${hours}:${minutes >= 10 ? minutes : '0'+minutes}`);
+                this.categories.push(`${hours >= 10 ? hours : '0'+hours}:${minutes >= 10 ? minutes : '0'+minutes}`);
 
                 minutes += 1;
             }
@@ -115,6 +161,9 @@ export default class WeatherCharts extends Vue {
         dataLabels: {
             enabled: false,
         },
+        stroke: {
+            curve: 'smooth',
+        }
     };
 }
 </script>
